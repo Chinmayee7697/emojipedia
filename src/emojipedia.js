f201e36d5e155ffde6022e51b9f2743aa2cac2d8
Chinmayee Sharma <chinmayeesharma911@gmail.com>
2024-02-29 16:21:21 +0530

    first commit

---
+++ src/emojipedia.js
@@ -0,0 +1,70 @@
+const emojipedia = [
+    {
+      id: 1,
+      emoji: "💪",
+      name: "Tense Biceps",
+      meaning:
+        "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+    },
+    {
+      id: 2,
+      emoji: "🙏",
+      name: "Person With Folded Hands",
+      meaning:
+        "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+    },
+    {
+      id: 3,
+      emoji: "🤣",
+      name: "Rolling On The Floor, Laughing",
+      meaning:
+        "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+    },
+    {
+        id: 4,
+        emoji: "😀",
+        name: "Grinning Face",
+        meaning:
+          "This expresses joy, happiness and satisfaction. It is often used to simply say hello, to express joy or excitement about something, or to break up a short text. "
+    },
+    {
+        id: 5,
+        emoji: "🥹",
+        name: "Face Holding Back Tears",
+        meaning:
+          "This shows a slightly smiling face with tears in its eyes, can express various emotions such as gratitude, emotion, or pride. "
+    },
+    {
+        id: 6,
+        emoji: "😅",
+        name: "Smiling Face With Open Mouth & Cold Sweat",
+        meaning:
+          "This is used to express relief that a tricky situation has been successfully handled or just gone well, such as an important meeting. It is also sometimes used to express nervousness or tension about upcoming difficult events."
+    },
+    {
+        id: 7,
+        emoji: "🏸",
+        name: "Badminton racket and shuttlecock",
+        meaning:
+          "I love badminton! The goal is to get the shuttlecock in the opposing field without leaving a possibility to beat it back. Competitive sport and popular leisure activity."
+    },
+    {
+        id: 8,
+        emoji: "🏏",
+        name: "Cricket bat and ball",
+        meaning:
+          "Cricket is a rounders game with two teams. Resembles baseball. The only sport with an official lunch break as well as tea breaks."
+    },
+    {
+        id: 9,
+        emoji: "💛",
+        name: " Yellow Heart",
+        meaning:
+          "A heart of gold. Yellow represents warmth, brightness and energy, so the yellow heart 💛 is often associated with positive feelings."
+    }
+
+
+  ];
+
+  export default emojipedia;
+  
